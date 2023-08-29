f6e89406ecd1ee09ea7304e6461a54d0339c3cb3
olvalaskevich <olvol269@gmail.com>
2023-08-29 15:09:40 +0300

    add Main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 
 import {Header} from "./layout/header/Header";
+import {Main} from "./layout/sections/main/Main";
 
 
 function App() {
     return (
         <div>
             <Header/>
+            <Main/>
         </div>
     );
 }
