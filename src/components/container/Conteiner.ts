51b0fc9ddd2b477f03045ad2a1739725a63e5551
olvalaskevich <olvol269@gmail.com>
2023-09-08 11:02:14 +0300

    create container

--- src/components/container/Conteiner.ts
+++ src/components/container/Conteiner.ts
@@ -2,5 +2,10 @@ import styled from "styled-components";
 
 
 export const Container=styled.div`
-max-width: 1190px;
+  max-width: 1190px;
+  width: 100%;
+  min-height: 100%;
+  margin: 0 auto;
+  padding: 0 15px;
+  border: 1px solid;
 `
