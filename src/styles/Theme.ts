926e2fb2ef4637232f33e57389068786dbfde813
olvalaskevich <olvol269@gmail.com>
2023-09-08 11:00:59 +0300

    add new colors

---
+++ src/styles/Theme.ts
@@ -0,0 +1,16 @@
+export const theme={
+    colors:{
+        primaryBg: '#FFF',
+        secondaryBg: '#191919',
+        fontTitle: '#42446E',
+        fontText:'#666',
+        fontLinkProject: '#000',
+        fontTitleContact: '#1E0E62',
+        fontSmallAbout: '#A7A7A7',
+        buttonSuccess: '#D7FFE0',
+        buttonText: '#018C0F',
+        gradientMain: '#E70FAA',
+        gradientStart: '#00C0FD',
+        gradientEnd: '#13B0F5'
+    }
+}
