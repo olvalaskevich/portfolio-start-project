2e84de208bee24631840678b7e69cf28b6610117
olvalaskevich <olvol269@gmail.com>
2023-09-12 09:02:36 +0300

    add media break points

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -12,5 +12,10 @@ export const theme={
         gradientMain: '#E70FAA',
         gradientStart: '#00C0FD',
         gradientEnd: '#13B0F5'
+    },
+
+    media:{
+        tablet:"screen and (max-width:768px)",
+        mobile:"screen and (max-width:576px)",
     }
 }
