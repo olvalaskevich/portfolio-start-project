7ec3d4835a7da63190cde5ff1312e5685fc76c60
olvalaskevich <olvol269@gmail.com>
2023-09-16 10:05:21 +0300

    add properties

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import iconSprite from '../../accets/images/sprite.svg'
+import styled from "styled-components";
 
 type IconPropsType= {
     iconId: string;
@@ -10,9 +11,14 @@ type IconPropsType= {
 }
 export const Icon = (props:IconPropsType) => {
     return (
-        <svg width={props.width || "97"} height={props.height || "59"} viewBox={props.viewBox || "0 0 97 59"} xmlns="http://www.w3.org/2000/svg">
+        <StyledSvg width={props.width || "97"} height={props.height || "59"} viewBox={props.viewBox || "0 0 97 59"} xmlns="http://www.w3.org/2000/svg">
             <use xlinkHref={`${iconSprite}#${props.iconId}`}/>
-        </svg>
+        </StyledSvg>
     );
 };
 
+
+const StyledSvg=styled.svg`
+  align-self: center;
+  justify-self: center;
+`
