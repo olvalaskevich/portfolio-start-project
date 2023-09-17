27798c457ff66c0c5eff4990296edf1ec23893ae
olvalaskevich <olvol269@gmail.com>
2023-09-17 20:57:02 +0300

    add padding in props and change max-width

--- src/components/container/Conteiner.ts
+++ src/components/container/Conteiner.ts
@@ -2,11 +2,18 @@ import styled from "styled-components";
 import {theme} from "../../styles/Theme";
 
 
-export const Container=styled.div`
-  max-width: 1190px;
+
+type ContainerPropsType={
+    padding?:string;
+}
+
+export const Container=styled.div<ContainerPropsType> `
+  max-width: 1193px;
   width: 100%;
   min-height: 100%;
   margin: 0 auto;
+  padding: ${props => props.padding||0};
+  
 
   @media ${theme.media.tablet}{
     padding: 15px;
