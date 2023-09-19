ea236d910022be0d78f12898350b4bb007f0a144
olvalaskevich <olvol269@gmail.com>
2023-09-19 20:39:07 +0300

    del unused component

--- src/components/logo/Logo.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {Icon} from "../icon/Icon";
-
-export const Logo = () => {
-    return (
-        <Icon iconId={'git'} width={"30"} height={"29.26"} viewBox={"0 0 30 29.26"}/>
-    );
-};
-
