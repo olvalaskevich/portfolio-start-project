c1f20e9b453980136d6f3a8b4beebf3b6b4c1ff4
olvalaskevich <olvol269@gmail.com>
2023-09-20 18:27:41 +0300

    add new component for slider for projects on mobile

---
+++ src/layout/sections/projects/SliderMobile.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import {Project} from "./Project";
+import Picture1 from '../../../accets/images/picture1.webp';
+import Picture2 from '../../../accets/images/picture2.webp';
+import Picture3 from '../../../accets/images/picture3.webp';
+import Picture4 from '../../../accets/images/picture4.webp';
+import Picture5 from '../../../accets/images/picture5.webp';
+import Picture6 from '../../../accets/images/picture6.webp';
+import '../../../styles/Slider.css';
+import {Container} from "../../../components/container/Conteiner";
+
+
+
+const items = [
+    <Project src={Picture1} alt={'pic1'}/>,
+    <Project src={Picture2} alt={'pic2'}/>,
+    <Project src={Picture3} alt={'pic3'}/>,
+    <Project src={Picture4} alt={'pic4'}/>,
+    <Project src={Picture5} alt={'pic5'}/>,
+    <Project src={Picture6} alt={'pic6'}/>
+];
+export const SliderMobile:React.FC = () => (
+<Container>
+    <AliceCarousel
+        mouseTracking
+        items={items}
+        autoWidth
+        autoHeight
+    />
+</Container>
+);
+
+
