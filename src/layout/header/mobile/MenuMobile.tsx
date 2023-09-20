2d1d08f550a4ab2ce9467632f91a5df350f9e51a
olvalaskevich <olvol269@gmail.com>
2023-09-20 13:11:16 +0300

    add onClick for burger menu

--- src/layout/header/mobile/MenuMobile.tsx
+++ src/layout/header/mobile/MenuMobile.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {List} from "../menu/List";
 import {S} from '../Header_Styles'
 
 export const MenuMobile:React.FC = () => {
+    const[menuIsOpen, setMenuIsOpen]=useState(false)
+    const onBurgerBtnClick= () => {setMenuIsOpen(!menuIsOpen)}
+
     return (
         <S.MenuWrapper>
-
-
-            <S.MenuMobile isOpen={false}>
+            <S.MenuMobile isOpen={menuIsOpen} onClick={()=>{setMenuIsOpen(false)}}>
                 <List/>
             </S.MenuMobile>
-            <S.BurgerButton isOpen={false}></S.BurgerButton>
+            <S.BurgerButton isOpen={menuIsOpen} onClick={onBurgerBtnClick}></S.BurgerButton>
         </S.MenuWrapper>
     );
 };
