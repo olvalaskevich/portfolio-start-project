acaa19a35b9332e81a3577bad7bcc2e796edc52d
olvalaskevich <olvol269@gmail.com>
2023-09-21 13:40:25 +0300

    add typewriter and title library

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import photo from '../../../accets/images/photo.jpg'
 import {Container} from "../../../components/container/Conteiner";
-import {S} from './Main_Styles'
+import {S} from './Main_Styles';
+import Tilt from 'react-parallax-tilt';
+import Typewriter from 'typewriter-effect';
 
 
 export const Main:React.FC = () => {
@@ -14,14 +16,22 @@ export const Main:React.FC = () => {
                 <S.String>My name is</S.String>
 
                 <S.GradientText>
-                 Pavan MG<br/>
+                    <Typewriter
+                        options={{
+                            strings: ['Pavan MG'],
+                            autoStart: true,
+                            loop: true,
+                        }}
+                    />
                 </S.GradientText>
 
                 <S.String>I build things for web</S.String>
             </S.TextBlock>
+            <Tilt tiltEnable={false} glareEnable={true} glareMaxOpacity={0.8} glareColor="white" glarePosition="all">
             <S.Border>
             <S.Photo src={photo} alt='Photo'/>
             </S.Border>
+            </Tilt>
         </S.MainWrapper>
             </Container>
         </S.Main>
