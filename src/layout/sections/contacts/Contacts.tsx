30d23c018192d41c132fecd71928de71f05d5b69
olvalaskevich <olvol269@gmail.com>
2024-07-16 21:00:40 +0300

    change structure of code

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -3,12 +3,10 @@ import {FlexWrapper} from "../../../components/flexwrapper/FlexWrapper";
 import {StyledTitle2} from "../../../components/titel2styled/Title2.styled";
 import {Container} from "../../../components/container/Conteiner";
 import {theme} from "../../../styles/Theme";
-import {Logo1} from "../../../components/logo/Logo1";
 import {Networks} from "../../../components/networks/Networks";
 import {Sf} from "../../footer/Footer_Styles";
 import {S} from './Contacts_Styles'
 import {Fade, Rotate, Zoom} from "react-awesome-reveal";
-// import Mailto from "react-mailto-link";
 
 
 
@@ -24,26 +22,22 @@ export const Contacts:React.FC<{footer?:boolean}>= () => {
                     For any questions please mail me:
                 </StyledTitle2>
                     <Rotate>
-                        <S.Link href={'mailto:hi@pavanmg.in'}>hi@pavanmg.in</S.Link>
+                        <S.Link href={'mailto:olvol@gmail.com'}>olvol@gmail.com</S.Link>
                     </Rotate>
                 {/*<Mailto email="hi@pavanmg.in" obfuscated={true}><S.Link>hi@pavanmg.in</S.Link></Mailto>*/}
                 </Zoom>
 
                 <S.ContactsAdaptive>
 
-                    <S.LogoAdaptive>
-                        <Fade triggerOnce={true}>
-                        <Logo1 footer/>
-                        </Fade>
-                    </S.LogoAdaptive>
+
 
                     {/*<Sf.FooterLink>+91 12345 09876</Sf.FooterLink>*/}
 
                     {/*<Sf.FooterLink>info@example.com</Sf.FooterLink>*/}
 
-                    <Sf.FooterLink><Fade triggerOnce={true}>+91 12345 09876</Fade></Sf.FooterLink>
+                    <Sf.FooterLink><Fade triggerOnce={true}>+375 33 XXX-XX-XX</Fade></Sf.FooterLink>
 
-                    <Sf.FooterLink><Fade triggerOnce={true}>info@example.com</Fade></Sf.FooterLink>
+                    <Sf.FooterLink><Fade triggerOnce={true}>olvol@gmail.com</Fade></Sf.FooterLink>
                     <Networks footer/>
 
                 </S.ContactsAdaptive>
